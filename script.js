613ad5ab7690b172f669970927fc7177c4ef8591
Ivan Ilnitskyi [OPONEO.PL S.A.] <iilnitskyi@opony.local>
2022-06-09 10:47:08 +0200

    working with localStorage

--- script.js
+++ script.js
@@ -75,7 +75,13 @@ class App {
   #workouts = [];
 
   constructor() {
+      // Get user's position
     this._getPosition();
+
+    // Get data from local starage
+    this._getLocalStorage();
+
+    // Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -104,6 +110,10 @@ class App {
 
     // Handling clicks on map
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+        this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -182,6 +192,9 @@ class App {
 
     // Hide form & clear input fields
     this._hideForm();
+
+    // Set local starage to all workouts
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -255,15 +268,12 @@ class App {
 
   _moveToPopup(e) {
     const workoutEl = e.target.closest('.workout');
-    console.log(workoutEl);
 
     if (!workoutEl) return;
 
     const workout = this.#workouts.find(
       work => work.id === workoutEl.dataset.id
     );
-
-    console.log(workout);
 
     this.#map.setView(workout.coords, this.#mapZoomLevel, {
       animate: true,
@@ -273,7 +283,28 @@ class App {
     });
 
     // using the public interface
-    workout.click();
+    //workout.click();
+  }
+
+  _setLocalStorage() {
+      localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage(){
+      const data = JSON.parse(localStorage.getItem('workouts'));
+
+      if(!data) return;
+
+      this.#workouts = data;
+
+      this.#workouts.forEach(work => {
+          this._renderWorkout(work);
+      });
+  }
+
+  reset() {
+      localStorage.removeItem('workouts');
+      location.reload();
   }
 }
 
